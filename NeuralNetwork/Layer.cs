5cfa4a45aa9d834d5d478d845eb7d9667a81e2d2
Unknown Eugene <kailas11252001@gmail.com>
2021-10-31 14:03:24 +0300

    Add project files.

---
+++ NeuralNetwork/Layer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetwork
+{
+    public class Layer
+    {
+        public List<Neuron> Neurons { get; set; }
+        public int Count => Neurons?.Count ?? 0;
+
+        public Layer(List<Neuron> neurons, NeuronType type = NeuronType.Normal)
+        {
+            //TODO: Implement input neurons for type correspondence
+
+            Neurons = neurons;
+        }
+
+        public List<double> GetSignals()
+        {
+            var result = new List<double>();
+
+            foreach(var neuron in Neurons)
+            {
+                result.Add(neuron.Output);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ NeuralNetwork/Topology.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace NeuralNetwork
+{
+    public class Topology
+    {
+        public int InputCount { get; }
+        public int OutputCount { get; }
+        public List<int> HiddenLayers { get; }
+
+        public Topology(int inputCount, int outputCOunt, params int[] hiddenLayersCount)
+        {
+            InputCount = inputCount;
+            OutputCount = outputCOunt;
+            HiddenLayers = new List<int>();
+
+            HiddenLayers.AddRange(hiddenLayersCount);
+        }
+    }
+}
